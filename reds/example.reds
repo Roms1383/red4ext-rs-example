e56e6d8ea6d3476cde9ba0cb961c84a09f8d494b
Roms1383 <romain.kelifa@gmail.com>
2023-04-25 09:10:42 +0700

    :alembic: experiments with macros and latest additions

--- reds/example.reds
+++ reds/example.reds
@@ -3,3 +3,15 @@ native func SumInts(ints: array<Int32>) -> Int32;
 native func PluginName() -> String;
 native func CreateTweakDBID(name: String) -> TweakDBID;
 native func AppendToTweakDBID(base: TweakDBID, suffix: String) -> TweakDBID;
+native func Consume(consumptions: ref<Consumptions>, consumable: Consumable) -> Void;
+
+public class Consumptions {
+ public func ConsumeReds(consumable: Consumable) -> Void {
+  LogChannel(n"DEBUG", s"called with consumable \(ToString(consumable)) \(consumable) \(EnumInt(consumable))");
+ }
+ static func New() -> ref<Consumptions> { return new Consumptions(); }
+}
+enum Consumable {
+ MaxDOC = 0,
+ BounceBack = 1,
+}
--- src/lib.rs
+++ src/lib.rs
@@ -9,14 +9,15 @@ define_plugin! {
         register_function!("PluginName", plugin_name);
         register_function!("CreateTweakDBID", create_tweakdb_id);
         register_function!("AppendToTweakDBID", append_to_tweakdb_id);
+        register_function!("Consume", consume);
     }
 }
 
 /// SumInts
 /// 
 /// test in CET like:
-/// ```swift
-/// LogChannel(CName.new("DEBUG"), ToString(SumInts([2000, 77])));
+/// ```lua
+/// LogChannel(CName.new("DEBUG"), SumInts({2000, 77}));
 /// ```
 fn sum_ints(ints: Vec<i32>) -> i32 {
     ints.iter().sum()
@@ -25,7 +26,7 @@ fn sum_ints(ints: Vec<i32>) -> i32 {
 /// PluginName
 /// 
 /// test in CET like:
-/// ```swift
+/// ```lua
 /// LogChannel(CName.new("DEBUG"), PluginName());
 /// ```
 fn plugin_name() -> String {
@@ -35,8 +36,8 @@ fn plugin_name() -> String {
 /// CreateTweakDBID
 /// 
 /// test in CET like:
-/// ```swift
-/// LogChannel(CName.new("DEBUG"), CreateTweakDBID("A.Test"));
+/// ```lua
+/// LogChannel(CName.new("DEBUG"), TDBID.ToStringDEBUG(CreateTweakDBID("A.Test")));
 /// ```
 fn create_tweakdb_id(name: String) -> TweakDBID {
     TweakDBID::new(&name)
@@ -46,9 +47,47 @@ fn create_tweakdb_id(name: String) -> TweakDBID {
 /// AppendToTweakDBID
 /// 
 /// test in CET like:
-/// ```swift
+/// ```lua
 /// LogChannel(CName.new("DEBUG"), TDBID.Create("A.Test") == AppendToTweakDBID(TDBID.Create("A."), "Test"));
 /// ```
 fn append_to_tweakdb_id(base: TweakDBID, suffix: String) -> TweakDBID {
     TweakDBID::new_from_base(&base, suffix.as_str())
+}
+
+/// Consume
+/// 
+/// test in CET like:
+/// ```lua
+/// Consume(NewObject("Consumptions"), Consumable.BlackLace);
+/// ```
+fn consume(consumptions: Ref<IScriptable>, consumable: Consumable) {
+    let o: Consumptions = Consumptions(consumptions);
+    o.consume_rust(consumable);
+}
+
+#[derive(Clone, Default)]
+#[repr(transparent)]
+struct Consumptions(Ref<IScriptable>);
+
+#[redscript_import]
+impl Consumptions {
+    pub fn consume_reds(&self, consumable: Consumable) -> ();
+}
+
+impl Consumptions {
+    fn consume_rust(&self, consumable: Consumable) {
+        self.consume_reds(consumable);
+    }
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+#[repr(i64)]
+pub enum Consumable {
+    #[default]
+    MaxDOC = 0,
+    BounceBack = 1,
+}
+
+unsafe impl NativeRepr for Consumable {
+    const NAME: &'static str = "Consumable";
 }
